3fec87f3d3819a25f7537ba73d3036b673032fcb
bank_npru <punsan.somkla@gmail.com>
2026-01-29 18:27:28 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css';
+
+import {
+  useEffect,
+  useState,
+} from 'react';
+
+function App() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  useEffect(() => {
+    inputValue === "hello" ? alert("Hello to you too!") : null;
+  }, [inputValue]);
+  return (
+    <>
+      <div className='p-6'>
+        <div className="grid grid-cols-4">
+          <h1 className="text-lg font-bold text-red-500">hello world</h1>
+          <h1 className="text-lg font-bold text-red-500">hello world</h1>
+          <h1 className="text-lg font-bold text-red-500">hello world</h1>
+          <h1 className="text-lg font-bold text-red-500">hello world</h1>
+          <h1 className="text-lg font-bold text-red-500">hello world</h1>
+          <h1 className="text-lg font-bold text-red-500">hello world</h1>
+          <h1 className="text-lg font-bold text-red-500">hello world</h1>
+          <h1 className="text-lg font-bold text-red-500">hello world</h1>
+          <input
+            onChange={handleChange}
+            type="text"
+            placeholder="Type here"
+            className="input"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
